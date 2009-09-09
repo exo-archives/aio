e13355b29071653a3008eee8da3336eb2c0e5dcd
Brice Revenant <brice.revenant@exoplatform.com>
2009-09-09 11:25:08 +0000

    premier depot de vinaworks

---
+++ Readme-Vinaworks-trunk.txt
@@ -0,0 +1,2 @@
+1- to build the AllInOne product , use this command :
+exobuild --product=allinone --version=1.3 --enable-workflow=jbpm --build --deploy
---
+++ web/vinaworkswebos/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,3 @@
+SkinService.addPortalSkin("WebOSSkin", "Default", "/vinaworkswebos/skin/Defaultskin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("WebOSSkin", "Mac", "/vinaworkswebos/skin/MacSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("WebOSSkin", "Vista", "/vinaworkswebos/skin/VistaSkin/Stylesheet.css", ServletContext); 
