3360f61d9058eea084817a3f28e1f4a7336dc507
Philippe  Aristote <philippe.aristote@exoplatform.com>
2009-09-29 11:31:35 +0000

    ALL-49: add conf to integrate webos. ALL-55: update product version in pom.

---
+++ web/portal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.exoplatform.portal.webui.portal.PortalTemplateConfigOption ;
+import org.exoplatform.webui.core.model.SelectItemCategory;
+
+List options = new ArrayList();  
+  SelectItemCategory acme = new SelectItemCategory("ACMESite");
+  acme.addSelectItemOption(
+      new PortalTemplateConfigOption("ACME Site", "acme", "ACME Site", "ACMESite").addGroup("/platform/administrators")
+  );  
+  
+    SelectItemCategory guest = new SelectItemCategory("ClassicPortal");
+  guest.addSelectItemOption(
+      //new PortalTemplateConfigOption("", "site", "Site Portal", "SitePortal").addGroup("/guest")
+      new PortalTemplateConfigOption("", "classic", "Classic Portal", "ClassicPortal").addGroup("/platform/guests")
+  );
+ 
+
+  SelectItemCategory webos = new SelectItemCategory("WebOSPortal");
+  webos.addSelectItemOption(
+    new PortalTemplateConfigOption("", "webos", "WebOS Portal", "WebOSPortal").addGroup("/platform/guests")
+  );
+  
+  options.add(webos);
+  options.add(guest);
+  options.add(acme);
+  
+return options ;
